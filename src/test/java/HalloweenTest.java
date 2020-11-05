645fc6ad71eecf228b17e81abe28255719fbb61d
zdemir1 <zaferdemir1200@gmail.com>
2020-11-05 17:38:31 -0500

    no treat or candy this year

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -1,5 +1,12 @@
 public class HalloweenTest {
 
     // I am about to give candy to everyone for halloween
+    // Are you here
+    // lets play game
+    // its fun
+
+    // there will be candy this year
+    // stay home
+
 
 }
